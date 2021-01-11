ee6e37a190919c4333190c85fa969f24b9d8a0a7
dafei <8319698@gmail.com>
2021-01-11 18:30:33 +0800

    提交

--- lib/main.dart
+++ lib/main.dart
@@ -17,25 +17,45 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'nevigration',
-          onPressed: () => debugPrint('侧边栏菜单'),
-        ),
-        title: Text("泛黄的App"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'search',
-            onPressed: () => debugPrint('搜索'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'nevigration',
+            onPressed: () => debugPrint('侧边栏菜单'),
           ),
-        ],
-        elevation: 0.0,
+          title: Text("泛黄的App"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'search',
+              onPressed: () => debugPrint('搜索'),
+            ),
+          ],
+          elevation: 0.0,
+          bottom: TabBar(
+            unselectedLabelColor: Colors.black38,
+            indicatorColor: Colors.black54,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.local_activity)),
+              Tab(icon: Icon(Icons.local_airport)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128, color: Colors.black),
+            Icon(Icons.local_activity, size: 128, color: Colors.black),
+            Icon(Icons.local_airport, size: 128, color: Colors.black),
+          ],
+        ),
       ),
-      body: LiveViewDemo(),
     );
   }
 }

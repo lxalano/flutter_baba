b51e306afef50bd5267daa36bb9bdae5b89d897d
dafei <8319698@gmail.com>
2021-01-14 22:27:53 +0800

    第五节课学完了

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -7,12 +7,90 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          IconBadge(Icons.padding),
-          IconBadge(
-            Icons.poll,
-            size: 64.0,
+          ConstrainedBox(
+            constraints: BoxConstraints(minHeight: 200.0, maxWidth: 200.0),
+            child: Container(
+              color: Color.fromRGBO(6, 54, 255, 1.0),
+            ),
           ),
-          IconBadge(Icons.dashboard),
+        ],
+      ),
+    );
+  }
+}
+
+class StackDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: Alignment.topRight,
+            children: [
+              SizedBox(
+                width: 200.0,
+                height: 400.0,
+                child: Container(
+                  alignment: Alignment(1.0, 1.0),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(6, 54, 255, 1.0),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 32.0,
+              ),
+              SizedBox(
+                width: 152.0,
+                height: 30.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(6, 54, 255, 1.0),
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(colors: [
+                      Color.fromRGBO(7, 56, 255, 1.0),
+                      Color.fromRGBO(2, 34, 255, 1.0),
+                    ]),
+                  ),
+                  child: Icon(
+                    Icons.place_outlined,
+                    color: Colors.white,
+                    size: 32,
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 20.0,
+                top: 30,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 32,
+                ),
+              ),
+              Positioned(
+                right: 20.0,
+                top: 92.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 32,
+                ),
+              ),
+              Positioned(
+                right: 20.0,
+                top: 130.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 32,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );

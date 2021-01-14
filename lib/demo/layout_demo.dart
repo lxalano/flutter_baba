e257708bc1f98534b2d3eeadb7914b9f956521e4
dafei <8319698@gmail.com>
2021-01-14 15:17:06 +0800

    1

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -3,9 +3,21 @@ import 'package:flutter/material.dart';
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconBadge(Icons.padding),
+          IconBadge(Icons.poll),
+          IconBadge(Icons.dashboard),
+        ],
+      ),
+    );
   }
 }
+
+
 
 class IconBadge extends StatelessWidget {
   final IconData icon;
@@ -21,6 +33,9 @@ class IconBadge extends StatelessWidget {
         size: size,
         color: Colors.white,
       ),
+      width: size + 60,
+      height: size + 60,
+      color: Color.fromRGBO(3, 54, 255, 1.0),
     );
   }
 }
